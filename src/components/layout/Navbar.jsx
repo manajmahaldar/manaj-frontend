96a4c50a76978282bc0fdac30de8dcc0237c4d6f
ramiz180 <125174091+Hasibur123@users.noreply.github.com>
2026-03-29 08:27:18 +0530

    latest commit

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16 items-center">
                     <Link to="/" className="flex items-center">
-                        <img src={logoImg} alt="Logo" className="h-10 w-auto object-contain" />
+                        <img src={logoImg} alt="Logo" className="h-20 w-auto object-contain" />
                     </Link>
 
                     {/* Desktop Links */}
